4568b520b4c1a89a215dea1581f625631c8674f0
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2018-04-02 17:12:02 +0200

    Rewrite modules to class syntax
    Exclude physics modules for now

--- src/IsometricPlugin.js
+++ src/IsometricPlugin.js
@@ -20,9 +20,6 @@
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
-*
-* 
-*
 */
 
 /**
@@ -31,41 +28,45 @@
  * @license      {@link http://opensource.org/licenses/MIT|MIT License}
  */
 
+import Projector from './Projector';
+
+export const VERSION = '0.9.3';
+
+// Directional consts
+export const UP = 0;
+export const DOWN = 1;
+export const FORWARDX = 2;
+export const FORWARDY = 3;
+export const BACKWARDX = 4;
+export const BACKWARDY = 5;
+
+//  Type consts
+export const ISOSPRITE = 'isosprite';
+export const ISOARCADE = 'isoarcade';
+
 /**
- * @class Phaser.Plugin.Isometric
+ * @class IsometricPlugin
  * 
  * @classdesc
  * Isometric is a comprehensive axonometric plugin for Phaser which provides an API for handling axonometric projection of assets in 3D space to the screen.
  * The goal has been to mimic as closely as possible the existing APIs provided by Phaser for standard orthogonal 2D projection, but add a third dimension.
  * Also included is an Arcade-based 3D AABB physics engine, which again is closely equivalent in functionality and its API.
- * 
- * @constructor
- * @param {Phaser.Game} game The current game instance.
- * @param {number} isometricType - the isometric projection angle to use.
  */
-Phaser.Plugin.Isometric = function (game, parent, isometricType) {
+export default class IsometricPlugin {
+  /**
+   * @constructor
+   * @param {Phaser.Scene} scene The current scene instance
+   */
+  constructor(scene) {
+    this.projector = new Projector(scene, scene.isometricType);
+    scene.isometric = scene.isometric || this.projector;
+  }
 
-    isometricType = isometricType || Phaser.Plugin.Isometric.CLASSIC;
+  static register(PluginManager) {
+    PluginManager.register('IsometricPlugin', IsometricPlugin, 'isometric');
+  }
 
-    Phaser.Plugin.call(this, game, parent);
-    this.projector = new Phaser.Plugin.Isometric.Projector(this.game, isometricType);
-    //  Add an instance of Isometric.Projector to game.iso if it doesn't exist already
-    this.game.iso = this.game.iso || this.projector;
-};
-
-Phaser.Plugin.Isometric.prototype = Object.create(Phaser.Plugin.prototype);
-Phaser.Plugin.Isometric.prototype.constructor = Phaser.Plugin.Isometric;
-
-Phaser.Plugin.Isometric.VERSION = '0.9.3';
-
-//  Directional consts
-Phaser.Plugin.Isometric.UP = 0;
-Phaser.Plugin.Isometric.DOWN = 1;
-Phaser.Plugin.Isometric.FORWARDX = 2;
-Phaser.Plugin.Isometric.FORWARDY = 3;
-Phaser.Plugin.Isometric.BACKWARDX = 4;
-Phaser.Plugin.Isometric.BACKWARDY = 5;
-
-//  Type consts
-Phaser.Plugin.Isometric.ISOSPRITE = 'isosprite';
-Phaser.Plugin.Isometric.ISOARCADE = 'isoarcade';
+  addIsoSprite(x, y, z, key, frame, group) {
+    return Phaser.GameObjectFactory.prototype.isoSprite.call(this.scene.add, x, y, z, key, frame, group);
+  }
+}
